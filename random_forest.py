5368b90e34f271aa26402729e281585d0c444d21
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2024-05-08 17:07:09 +0200

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -84,11 +84,10 @@ n_estimators = [int(x) for x in np.linspace(start=10, stop=100, num=11)]
 max_features = ['log2', 'sqrt']
 # Maximum number of levels in tree
 max_depth = [int(x) for x in np.linspace(1, 30, num=6)]
-#max_depth.append(None)
 # Minimum number of samples required to split a node
-min_samples_split = [4, 10, 50]
+min_samples_split = [10, 80]
 # Minimum number of samples required at each leaf node
-min_samples_leaf = [2, 4, 8]
+min_samples_leaf = [4, 8]
 # Method of selecting samples for training each tree
 bootstrap = [True, False]  # Create the random grid
 
@@ -105,7 +104,7 @@ random_grid = {'n_estimators': n_estimators,
 # Random Forest Classifier
 rf = RandomForestClassifier()
 rfc = RandomizedSearchCV(estimator=rf, param_distributions=random_grid,
-                         n_iter=100, cv=3, verbose=1, random_state=42, n_jobs=-1)
+                         n_iter=100, cv=3, verbose=2, random_state=42, n_jobs=-1)
 rfc.fit(X_train, y_train)
 pprint(rfc.best_params_)
 # END COPY PASTED CODE
